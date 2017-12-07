010f74d1c52cef4c0ba432264069b1fc25dc0fd6
onestardev <faonestar514@gmail.com>
2017-12-07 22:28:21 +0100

    menu bar

--- resources/assets/js/pages/home.vue
+++ resources/assets/js/pages/home.vue
@@ -11,11 +11,7 @@
                      <li class="dropdown">
                       <a class="dropdown-toggle" data-toggle="dropdown" href="#">Portofolio
                       <span class="caret"></span></a>
-                      <ul class="dropdown-menu">
-                        <li><a href="#">Page 1-1</a></li>
-                        <li><a href="#">Page 1-2</a></li>
-                        <li><a href="#">Page 1-3</a></li>
-                      </ul>
+                      
                     </li>
                     <li><a href="#">Client</a></li>
                     <li><a href="#">Brokers</a></li>
